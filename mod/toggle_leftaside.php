ec360608f2ebc806959950f587c5450417292546
Jan David Mörike <david_moerike@arcor.de>
2014-06-01 17:19:43 +0200

    Hiding - on demand - sidebar on vier and vier-mobil
    The idea at present time is:
    
    By z-index: 3 for section, that is one higher than for aside,
    one will not see the sidebar, if the position of widget
    is left: 0px, or not given.
    
    Module toggle_leftaside sets an entry for to show sidebar, or deletes it.
    
    When sidebar - width is 215px - is to be shown, the position of
    section is set to left: 215
    
    Not everything ready.

--- mod/toggle_leftaside.php
+++ mod/toggle_leftaside.php
@@ -1,10 +1,22 @@
 <?php
-function network_init(&$a) {
+function toggle_leftaside_init(&$a) {
 	if(! local_user()) {
 		notice( t('Permission denied.') . EOL);
 		return;
 	}
 
-info( 'get_pconfig: ' . get_pconfig( local_user(), 'system', 'leftsidebar') ) ;
-}
+	    if( get_pconfig( local_user(), 'system', 'leftsidebar') ) 
+		del_pconfig( local_user(), 'system', 'leftsidebar') ;
+    
+	    else
+		set_pconfig( local_user(), 'system', 'leftsidebar', 'show') ;
+
+        logger( "get_pconfig: " . get_pconfig( local_user(), 'system', 'leftsidebar'), LOGGER_DEBUG) ;
+
+	if(isset($_GET['address']))
+		$address = $_GET['address'];
+	else
+		$address = $a->get_baseurl();
+
+	goaway($address);}
 ?>
--- view/theme/vier/default.php
+++ view/theme/vier/default.php
@@ -7,17 +7,8 @@
 </head>
 <body>
 	<?php if(x($page,'nav')) echo $page['nav']; ?>
-	<aside><?php if(x($page,'aside') 
-	  && get_pconfig( local_user(), 'system', 'leftsidebar') )
-	    echo $page['aside']; 
-info( 'get_pconfig: ' . get_pconfig( local_user(), 'system', 'leftsidebar') ) ;
-	?></aside>
-	<section><?php if(x($page,'content') )
-echo $page['content'];
-/* Next 2 lines for testing / debugging purpose; will soon be removed again */
-del_pconfig ( local_user(), 'system', 'leftsidebar') ;
-set_pconfig ( local_user(), 'system', 'leftsidebar', 'show' ) ;
- ?>
+	<aside><?php if(x($page,'aside')) echo $page['aside']; ?></aside>
+	<section><?php if(x($page,'content') ) echo $page['content']; ?>
 		<div id="page-footer"></div>
 	</section>
 	<right_aside><?php if(x($page,'right_aside')) echo $page['right_aside']; ?></right_aside>
