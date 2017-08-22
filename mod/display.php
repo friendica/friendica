39ed860bafb33647531c9109349fb15f74a67bb6
Alexandre Alapetite <alexandre@alapetite.fr>
2017-08-22 22:35:23 +0200

    SQL unseen is tinyint, not bool
    https://github.com/friendica/friendica/issues/3645

--- mod/display.php
+++ mod/display.php
@@ -414,9 +414,9 @@ function display_content(App $a, $update = 0) {
 		$s = dba::inArray($r);
 
 		if ((local_user()) && (local_user() == $a->profile['uid'])) {
-			$unseen = dba::select('item', array('id'), array('parent' => $s[0]['parent'], 'unseen' => true), array('limit' => 1));
+			$unseen = dba::select('item', array('id'), array('parent' => $s[0]['parent'], 'unseen' => 1), array('limit' => 1));
 			if (dbm::is_result($unseen)) {
-				dba::update('item', array('unseen' => false), array('parent' => $s[0]['parent'], 'unseen' => true));
+				dba::update('item', array('unseen' => 0), array('parent' => $s[0]['parent'], 'unseen' => 1));
 			}
 		}
 
--- mod/profile.php
+++ mod/profile.php
@@ -334,10 +334,10 @@ function profile_content(App $a, $update = 0) {
 
 
 	if ($is_owner) {
-		$unseen = dba::exists('item', array('wall' => true, 'unseen' => true, 'uid' => local_user()));
+		$unseen = dba::exists('item', array('wall' => true, 'unseen' => 1, 'uid' => local_user()));
 		if ($unseen) {
-			$r = dba::update('item', array('unseen' => false),
-					array('wall' => true, 'unseen' => true, 'uid' => local_user()));
+			$r = dba::update('item', array('unseen' => 0),
+					array('wall' => true, 'unseen' => 1, 'uid' => local_user()));
 		}
 	}
 
