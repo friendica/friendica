4f9f00ee106f6aa10ec2c978c73acb368c3ecfa8
Hypolite Petovan <mrpetovan@gmail.com>
2018-07-14 14:09:20 -0400

    Fix TOC display in mod/help

--- mod/help.php
+++ mod/help.php
@@ -69,30 +69,38 @@ function help_content(App $a) {
 	if ($filename !== "Home") {
 		// create TOC but not for home
 		$lines = explode("\n", $html);
-		$toc="<h2>TOC</h2><ul id='toc'>";
-		$lastlevel=1;
-		$idnum = [0,0,0,0,0,0,0];
-		foreach($lines as &$line){
-			if (substr($line,0,2)=="<h") {
-				$level = substr($line,2,1);
-				if ($level!="r") {
-					$level = intval($level);
-					if ($level<$lastlevel) {
-						for($k=$level;$k<$lastlevel; $k++) $toc.="</ul>";
-						for($k=$level+1;$k<count($idnum);$k++) $idnum[$k]=0;
+		$toc = '<h2>TOC</h2><ul id="toc">';
+		$lastlevel = 1;
+		$idnum = [0, 0, 0, 0, 0, 0, 0];
+		foreach ($lines as &$line) {
+			if (substr($line, 0, 2) == "<h") {
+				$level = substr($line, 2, 1);
+				if ($level != "r") {
+					$level = intval($level) - 1;
+					if ($level < $lastlevel) {
+						for ($k = $level; $k < $lastlevel; $k++) {
+							$toc .= "</ul></li>";
+						}
+						for ($k = $level + 1; $k < count($idnum); $k++) {
+							$idnum[$k] = 0;
+						}
 					}
-					if ($level>$lastlevel) $toc.="<ul>";
-					$idnum[$level]++;
-					$id = implode("_", array_slice($idnum,1,$level));
-					$href = System::baseUrl()."/help/{$filename}#{$id}";
-					$toc .= "<li><a href='{$href}'>".strip_tags($line)."</a></li>";
-					$line = "<a name='{$id}'></a>".$line;
+					if ($level > $lastlevel) {
+						$toc .= "<li><ul>";
+					}
+					$idnum[$level] ++;
+					$id = implode("_", array_slice($idnum, 1, $level));
+					$href = System::baseUrl() . "/help/{$filename}#{$id}";
+					$toc .= "<li><a href='{$href}'>" . strip_tags($line) . "</a></li>";
+					$line = "<a name='{$id}'></a>" . $line;
 					$lastlevel = $level;
 				}
 			}
 		}
-		for($k=0;$k<$lastlevel; $k++) $toc.="</ul>";
-		$html = implode("\n",$lines);
+		for ($k = 0; $k < $lastlevel; $k++) {
+			$toc .= "</ul>";
+		}
+		$html = implode("\n", $lines);
 
 		$a->page['aside'] = '<div class="help-aside-wrapper widget"><div id="toc-wrapper">' . $toc . '</div>' . $a->page['aside'] . '</div>';
 	}
