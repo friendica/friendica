7e8eb912dfb32e85c9f150f717a36bc938eb1f15
Jonny Tischbein <jonny_tischbein@systemli.org>
2018-09-19 19:29:36 +0200

    mod/message fix return url using message id

--- mod/message.php
+++ mod/message.php
@@ -92,7 +92,8 @@ function message_post(App $a)
 		$a->argc = 2;
 		$a->argv[1] = 'new';
 	} else {
-		goaway(System::baseUrl . '/' . $a->cmd);
+
+		goaway(System::baseUrl() . '/' . $a->cmd . '/' . $ret);
 	}
 }
 
