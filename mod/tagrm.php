2c5a46b4ca25c9bf5880a2b814e7ce3694612983
Jonny Tischbein <jonny_tischbein@systemli.org>
2018-10-23 10:51:53 +0200

    Fix adding / removing photo tags by - adding a Term::deleteAllTags function - changing the Item:update function for the field tag - adding the possiblity to remove tag via GET /tagrm/[id]/[hex(Tag)] - display photo tags like categories in post with delete button - fix tagrm return when no photo has no tag

--- mod/tagrm.php
+++ mod/tagrm.php
@@ -13,65 +13,89 @@ use Friendica\Model\Item;
 function tagrm_post(App $a)
 {
 	if (!local_user()) {
-		goaway(System::baseUrl() . '/' . $_SESSION['photo_return']);
+		goaway('/' . $_SESSION['photo_return']);
 	}
 
 	if (x($_POST,'submit') && ($_POST['submit'] === L10n::t('Cancel'))) {
-		goaway(System::baseUrl() . '/' . $_SESSION['photo_return']);
+		goaway('/' . $_SESSION['photo_return']);
 	}
 
-	$tag =  (x($_POST,'tag')  ? hex2bin(notags(trim($_POST['tag']))) : '');
-	$item_id = (x($_POST,'item') ? intval($_POST['item'])               : 0);
-
-	$item = Item::selectFirst(['tag'], ['id' => $item_id, 'uid' => local_user()]);
-	if (!DBA::isResult($item)) {
-		goaway(System::baseUrl() . '/' . $_SESSION['photo_return']);
-	}
-
-	$arr = explode(',', $item['tag']);
-	for ($x = 0; $x < count($arr); $x ++) {
-		if ($arr[$x] === $tag) {
-			unset($arr[$x]);
-			break;
+	$tags = [];
+	if (defaults($_POST, 'tag', '')){
+		foreach ($_POST['tag'] as $t){
+			array_push($tags, hex2bin(notags(trim($t))));
 		}
 	}
 
-	$tag_str = implode(',',$arr);
+	$item_id = defaults($_POST,'item', 0);
+	update_tags($item_id, $tags);
 
-	Item::update(['tag' => $tag_str], ['id' => $item_id]);
+	info(L10n::t('Tag(s) removed') . EOL );
 
-	info(L10n::t('Tag removed') . EOL );
-	goaway(System::baseUrl() . '/' . $_SESSION['photo_return']);
+	goaway('/' . $_SESSION['photo_return']);
 
 	// NOTREACHED
 }
 
+function update_tags($item_id, $tags){
+	if (empty($item_id) || empty($tags)){
+		goaway('/' . $_SESSION['photo_return']);
+	}
 
+	$item = Item::selectFirst(['tag'], ['id' => $item_id, 'uid' => local_user()]);
+	if (!DBA::isResult($item)) {
+		goaway('/' . $_SESSION['photo_return']);
+	}
+
+	$arr = explode(',', $item['tag']);
+
+	foreach ($tags as $t) {
+		foreach ($arr as $i => $x) {
+			if (strcmp($x, $t) == 0) {
+				unset($arr[$i]);
+				break;
+			}
+		}
+	}
+
+	$tag_str = implode(',',$arr);
+	if(empty($tag_str)){
+		$tag_str = '';
+	}
+
+	Item::update(['tag' => $tag_str], ['id' => $item_id]);
+
+}
 
 function tagrm_content(App $a)
 {
 	$o = '';
 
 	if (!local_user()) {
-		goaway(System::baseUrl() . '/' . $_SESSION['photo_return']);
+		goaway('/' . $_SESSION['photo_return']);
 		// NOTREACHED
+	}
+
+	if ($a->argc == 3){
+		update_tags($a->argv[1], [hex2bin(notags(trim($a->argv[2])))]);
+		goaway('/' . $_SESSION['photo_return']);
 	}
 
 	$item_id = (($a->argc > 1) ? intval($a->argv[1]) : 0);
 	if (!$item_id) {
-		goaway(System::baseUrl() . '/' . $_SESSION['photo_return']);
+		goaway('/' . $_SESSION['photo_return']);
 		// NOTREACHED
 	}
 
 	$item = Item::selectFirst(['tag'], ['id' => $item_id, 'uid' => local_user()]);
 	if (!DBA::isResult($item)) {
-		goaway(System::baseUrl() . '/' . $_SESSION['photo_return']);
+		goaway('/' . $_SESSION['photo_return']);
 	}
 
 	$arr = explode(',', $item['tag']);
 
-	if (!count($arr)) {
-		goaway(System::baseUrl() . '/' . $_SESSION['photo_return']);
+	if (empty($item['tag'])) {
+		goaway('/' . $_SESSION['photo_return']);
 	}
 
 	$o .= '<h3>' . L10n::t('Remove Item Tag') . '</h3>';
@@ -83,7 +107,7 @@ function tagrm_content(App $a)
 	$o .= '<ul>';
 
 	foreach ($arr as $x) {
-		$o .= '<li><input type="checkbox" name="tag" value="' . bin2hex($x) . '" >' . BBCode::convert($x) . '</input></li>';
+		$o .= '<li><input type="checkbox" name="tag[]" value="' . bin2hex($x) . '" >' . BBCode::convert($x) . '</input></li>';
 	}
 
 	$o .= '</ul>';
