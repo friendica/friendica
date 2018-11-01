e8312524cfa79113e7df3d6a0a30ea4c85fca16a
Vinzenz Vietzke <vinz@vinzv.de>
2018-11-01 21:52:42 +0100

    adding new brown color frio scheme "autumn"

---
+++ view/theme/frio/scheme/autumn.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Name: Automn
+ * Author: vinzv
+ *
+ * List here all variables which will get overwritten through this scheme
+ * Overwrites: nav_bg, nav_icon_color, link_color, background_color, contentbg_transp
+ */
+
+	$nav_bg = "#655a4a";
+	$nav_icon_color = "#fff";
+	$link_color = "#777777";
+	$background_color = "#ededed";
+	$login_bg_color = "#655a4a";
+	$login_bg_image = "scheme/autumn.jpg";
+	$contentbg_transp = 100;
