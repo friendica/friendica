7ec65ac94e9e698fd939f71b357c3e040206c8ce
Philipp Holzer <admin@philipp.info>
2019-01-19 19:11:27 +0100

    Mocking API

--- tests/include/ApiTestLegacy.php
+++ tests/include/ApiTestLegacy.php
@@ -85,33 +85,6 @@ class ApiTestLegacy extends ApiTest
 	}
 
 	/**
-	 * Test the api_format_data() function.
-	 * @return void
-	 */
-	public function testApiFormatData()
-	{
-		$data = ['some_data'];
-		$this->assertEquals($data, api_format_data('root_element', 'json', $data));
-	}
-
-	/**
-	 * Test the api_format_data() function with an XML result.
-	 * @return void
-	 */
-	public function testApiFormatDataWithXml()
-	{
-		$this->assertEquals(
-			'<?xml version="1.0"?>'."\n".
-			'<root_element xmlns="http://api.twitter.com" xmlns:statusnet="http://status.net/schema/api/1/" '.
-				'xmlns:friendica="http://friendi.ca/schema/api/1/" '.
-				'xmlns:georss="http://www.georss.org/georss">'."\n".
-				'  <data>some_data</data>'."\n".
-			'</root_element>'."\n",
-			api_format_data('root_element', 'xml', ['data' => ['some_data']])
-		);
-	}
-
-	/**
 	 * Test the api_account_verify_credentials() function.
 	 * @dataProvider dataApiUser
 	 * @return void
