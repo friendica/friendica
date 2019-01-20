00f1c84b552e84fda90b88022e694771f36bf90b
Philipp Holzer <admin@philipp.info>
2019-01-20 19:09:17 +0100

    Mocking API

--- tests/API/Function/Statuses/DestroyTest.php
+++ tests/API/Function/Statuses/DestroyTest.php
@@ -2,17 +2,29 @@
 
 namespace Friendica\Test\API\Statuses;
 
+use Friendica\Model\Item;
 use Friendica\Test\API\ApiTest;
+use Friendica\Test\Util\ApiUserItemDatasetTrait;
 
 class DestroyTest extends ApiTest
 {
+	use ApiUserItemDatasetTrait;
+
 	/**
 	 * Test the api_statuses_destroy() function.
+	 * @dataProvider dataApiUserItemFull
 	 * @return void
 	 * @expectedException Friendica\Network\HTTPException\BadRequestException
 	 */
-	public function testDefault()
+	public function testDefault($user, $item)
 	{
+		$this->mockApiUser($user['uid']);
+		$this->mockApiGetUser($user, 2);
+
+		$this->mockItemConstants();
+		$this->mockItemSelectFirst(Item::ITEM_FIELDLIST, [], [], $item, 1);
+		$this->mockIsResult($item, false, 1);
+
 		api_statuses_destroy('json');
 	}
 
@@ -23,7 +35,6 @@ class DestroyTest extends ApiTest
 	 */
 	public function testWithoutAuthenticatedUser()
 	{
-		$_SESSION['authenticated'] = false;
 		api_statuses_destroy('json');
 	}
 
