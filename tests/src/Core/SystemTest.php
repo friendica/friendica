99fdf5268bca8f8a093b08dd0a68776f818fb54f
Philipp Holzer <admin+github@philipp.info>
2019-06-10 11:58:20 +0200

    Refactor SystemTest without DB

--- tests/src/Core/SystemTest.php
+++ tests/src/Core/SystemTest.php
@@ -3,13 +3,27 @@
 namespace Friendica\Test\src\Core;
 
 use Friendica\Core\System;
-use PHPUnit\Framework\TestCase;
+use Friendica\Test\MockedTest;
+use Friendica\Test\Util\AppMockTrait;
+use Friendica\Test\Util\VFSTrait;
 
-class SystemTest extends TestCase
+class SystemTest extends MockedTest
 {
+	use VFSTrait;
+	use AppMockTrait;
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->setUpVfsDir();
+		$this->mockApp($this->root);
+
+		$this->app->shouldReceive('getHostName')->andReturn('localhost');
+	}
+
 	private function assertGuid($guid, $length, $prefix = '')
 	{
-		print $guid;
 		$length -= strlen($prefix);
 		$this->assertRegExp("/^" . $prefix . "[a-z0-9]{" . $length . "}?$/", $guid);
 	}
@@ -20,17 +34,20 @@ class SystemTest extends TestCase
 		$this->assertGuid($guid, 16);
 	}
 
-	function testGuidWithSize32() {
+	function testGuidWithSize32()
+	{
 		$guid = System::createGUID(32);
 		$this->assertGuid($guid, 32);
 	}
 
-	function testGuidWithSize64() {
+	function testGuidWithSize64()
+	{
 		$guid = System::createGUID(64);
 		$this->assertGuid($guid, 64);
 	}
 
-	function testGuidWithPrefix() {
+	function testGuidWithPrefix()
+	{
 		$guid = System::createGUID(23, 'test');
 		$this->assertGuid($guid, 23, 'test');
 	}
