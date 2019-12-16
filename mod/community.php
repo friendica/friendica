b63c5dfec213845635aec29909a98276623c2961
nupplaPhil <admin@philipp.info>
2019-12-16 01:12:07 +0100

    Remove deprecated App::is_mobile/is_tablet - replace with DI::mode()->isMobile()/isTablet()

--- mod/community.php
+++ mod/community.php
@@ -14,6 +14,7 @@ use Friendica\Core\L10n;
 use Friendica\Core\PConfig;
 use Friendica\Core\Renderer;
 use Friendica\Database\DBA;
+use Friendica\DI;
 use Friendica\Model\Item;
 use Friendica\Model\User;
 
@@ -135,7 +136,7 @@ function community_content(App $a, $update = 0)
 	}
 
 	// check if we serve a mobile device and get the user settings accordingly
-	if ($a->is_mobile) {
+	if (DI::mode()->isMobile()) {
 		$itemspage_network = PConfig::get(local_user(), 'system', 'itemspage_mobile_network', 20);
 	} else {
 		$itemspage_network = PConfig::get(local_user(), 'system', 'itemspage_network', 40);
