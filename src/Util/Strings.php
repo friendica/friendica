085fb391a739f569cf5a4ed057a71c007fb3cb1f
Marcus Funch <mfm@magenta.dk>
2025-06-25 17:49:09 +0200

    Make one character URLs clickable with a one character change

--- src/Util/Strings.php
+++ src/Util/Strings.php
@@ -396,7 +396,7 @@ class Strings
 		return 'https?://                   # http or https protocol
   (?:
 	[^/\s\xA0`!()\[\]{};:\'",<>?«»“”‘’.]    # Domain can\'t start with a .
-	[^/\s\xA0`!()\[\]{};:\'",<>?«»“”‘’]+    # Domain can\'t end with a .
+	[^/\s\xA0`!()\[\]{};:\'",<>?«»“”‘’]*    # Domain can\'t end with a .
 	\.
 	[^/\s\xA0`!()\[\]{};:\'".,<>?«»“”‘’]+/? # Followed by a slash
   )
