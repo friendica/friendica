e2748fce2f7f8b3b92e57db1e24df004de7bcd63
Art4 <art4@wlabs.de>
2026-08-17 21:21:49 +0200

    Add integration tests for Twitter\Users\Lookup

--- tests/src/Module/Api/Twitter/Users/LookupTest.php
+++ tests/src/Module/Api/Twitter/Users/LookupTest.php
@@ -24,6 +24,7 @@ class LookupTest extends ApiTestCase
 	{
 		$this->expectException(NotFoundException::class);
 
+		// @phpstan-ignore method.deprecated
 		(new Lookup(DI::mstdnError(), DI::appHelper(), DI::l10n(), DI::baseUrl(), DI::args(), DI::logger(), DI::profiler(), DI::apiResponse(), []))
 			->run($this->httpExceptionMock);
 	}
@@ -38,6 +39,7 @@ class LookupTest extends ApiTestCase
 		// @todo: This call is needed for this test
 		Renderer::registerTemplateEngine(\Friendica\Render\FriendicaSmartyEngine::class);
 
+		// @phpstan-ignore method.deprecated
 		$response = (new Lookup(DI::mstdnError(), DI::appHelper(), DI::l10n(), DI::baseUrl(), DI::args(), DI::logger(), DI::profiler(), DI::apiResponse(), []))
 			->run($this->httpExceptionMock, [
 				'user_id' => static::OTHER_USER['id'],
