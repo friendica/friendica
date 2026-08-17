f47b2ad8e968025298f3e92770853573ad3707a1
Art4 <art4@wlabs.de>
2026-08-17 21:45:19 +0200

    Add integration test for Twitter\DirectMessages\Conversation

---
+++ tests/Integration/Module/Api/Twitter/DirectMessages/ConversationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+// Copyright (C) 2010-2026, the Friendica project
+// SPDX-FileCopyrightText: 2010-2026 the Friendica project
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace Friendica\Test\Integration\Module\Api\Twitter\DirectMessages;
+
+use Friendica\DI;
+use Friendica\Factory\Api\Twitter\DirectMessage;
+use Friendica\Module\Api\Twitter\DirectMessages\Conversation;
+use Friendica\Test\ApiTestCase;
+use GuzzleHttp\Psr7\ServerRequest;
+
+final class ConversationTest extends ApiTestCase
+{
+	public function testApiDirectMessagesBoxWithConversation(): void
+	{
+		$directMessage = new DirectMessage(DI::logger(), DI::dba(), DI::twitterUser());
+
+		$module = new Conversation($directMessage, DI::dba(), DI::mstdnError(), DI::appHelper(), DI::l10n(), DI::baseUrl(), DI::args(), DI::logger(), DI::profiler(), DI::apiResponse(), [], ['extension' => 'json']);
+
+		$request = (new ServerRequest('GET', 'https://friendica.local/api/direct_messages/conversation'))
+			->withQueryParams(['format' => 'json', 'friendica_verbose' => 'true']);
+
+		$response = $module->handleRequest($request);
+
+		self::assertEquals(200, $response->getStatusCode());
+
+		$json = $this->toJson($response);
+
+		self::assertEquals('error', $json->result);
+		self::assertEquals('no mails available', $json->message);
+	}
+}
