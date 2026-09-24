961eeebd77936c67cf3035f8c263fa7192c394c6
Marcus F. <marcx@protonmail.com>
2026-09-24 21:48:51 +0200

    Make Vier profile link go to Posts (conversations) instead

--- src/Content/Nav.php
+++ src/Content/Nav.php
@@ -77,8 +77,9 @@ class Nav
 
 		$nav_info = $this->getInfo();
 
+		// NOTE: Frio does not use this link.
 		if ($this->session->getLocalUserNickname()) {
-			$profile_link = 'profile/' . $this->session->getLocalUserNickname() . '/profile';
+			$profile_link = 'profile/' . $this->session->getLocalUserNickname() . '/conversations';
 		} else {
 			$profile_link = false;
 		}
